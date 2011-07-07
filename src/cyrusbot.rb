0631915b7410959c137fe63b1cad441200bb68b2
Jesse Chen <jchen@cyrusinnovation.com>
2011-07-07 17:48:22 -0400

    yaml configuration

--- src/cyrusbot.rb
+++ src/cyrusbot.rb
@@ -1,12 +1,15 @@
 require "twitter"
+require "yaml"
 
-#todo load from credentials.yml instead
+File.open(File.dirname(__FILE__) + "/credentials.yml") do |credentials_file| 
+  @credentials = YAML.load(credentials_file)
+end 
 
 Twitter.configure do |config|
-  config.consumer_key = "LtOHVDSvhnDowChkHPaQ"
-  config.consumer_secret = "slzYcUFLyY5b4igabsiywjaWSl6DO4SUtidLJR6Lfjs"
-  config.oauth_token = "330381202-81Eak499zvhHfONzZ0sm0eQAfzix7sUITuZGt4C6"
-  config.oauth_token_secret = "Nd5UCwPqkg07PfEVGlHrgksJzcMoTG5OMaMkVLV7c"
+  config.consumer_key = @credentials["consumer_key"]
+  config.consumer_secret = @credentials["consumer_secret"] 
+  config.oauth_token = @credentials["oauth_token"]
+  config.oauth_token_secret = @credentials["oauth_token_secret"] 
 end
 
 @client = Twitter::Client.new
